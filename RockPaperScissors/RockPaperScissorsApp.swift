1f8e2ca2bdd47f1db80459b4503fc42ef3c77b04
Adrianus Pandu Wicaksono <panduadrianus@gmail.com>
2025-03-28 13:52:55 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by student on 28.03.25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
